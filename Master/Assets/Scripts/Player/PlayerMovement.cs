696d14a6abaf2e5b778f6b4726c99be3ecf57203
Mockyy <32643968+Mockyy@users.noreply.github.com>
2021-01-04 11:09:25 +0100

    Mouvement et saut

---
+++ Master/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    protected Rigidbody2D rb;
+
+    protected float movement;
+    [SerializeField] protected float speed = default;
+    [SerializeField] protected float jumpForce = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+
+        rb.position += new Vector2(movement * speed * Time.deltaTime, 0);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
