4d385cac536741ddcba70c049393a8e72f532f1b
Mockyy <32643968+Mockyy@users.noreply.github.com>
2021-05-10 13:32:58 +0200

    Playtest

---
+++ Master/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float xOffset;
+    [SerializeField] float yOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10);
+    }
+}
